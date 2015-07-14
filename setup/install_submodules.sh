35ea57cccc1ea8417ad63fa1b14ef3f811810185
Геннадий Александров <g.aleksandrov@timeweb.ru>
2015-07-14 18:40:16 +0300

    add vim-puppet

--- setup/install_submodules.sh
+++ setup/install_submodules.sh
@@ -16,6 +16,7 @@ git submodule add https://github.com/reinh/vim-makegreen bundle/makegreen
 git submodule add https://github.com/vim-scripts/TaskList.vim.git bundle/tasklist
 git submodule add https://github.com/vim-scripts/The-NERD-tree.git bundle/nerdtree
 git submodule add https://github.com/scrooloose/syntastic.git bundle/syntastic
+git submodule add -f git://github.com/rodjek/vim-puppet.git bundle/puppet
 git submodule init
 git submodule update
 git submodule foreach git submodule init
