90221655c694146fec7c8ac55805c55bd6c12aaf
Геннадий Александров <g.aleksandrov@timeweb.ru>
2015-09-21 16:23:23 +0300

    remove extra plugins

--- setup/install_submodules.sh
+++ setup/install_submodules.sh
@@ -10,7 +10,6 @@ git submodule add https://github.com/vim-scripts/TaskList.vim.git bundle/tasklis
 git submodule add https://github.com/vim-scripts/The-NERD-tree.git bundle/nerdtree
 git submodule add https://github.com/scrooloose/syntastic.git bundle/syntastic
 git submodule add -f git://github.com/rodjek/vim-puppet.git bundle/puppet
-git submodule add https://github.com/davidhalter/jedi-vim bundle/jedi-vim
 git submodule init
 git submodule update
 git submodule foreach git submodule init
