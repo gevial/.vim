836f3812e8c0ad822d7ef767dfee33dfbdaf1864
Gennady Aleksandrov <gevial@users.noreply.github.com>
2015-12-27 19:43:47 +0300

    remove some stuff

--- setup/install_submodules.sh
+++ setup/install_submodules.sh
@@ -3,10 +3,7 @@
 cd ~/.vim
 
 git submodule add http://github.com/tpope/vim-fugitive.git bundle/fugitive
-git submodule add https://github.com/msanders/snipmate.vim.git bundle/snipmate
-git submodule add https://github.com/ervandew/supertab.git bundle/supertab
 git submodule add https://github.com/sjl/gundo.vim.git bundle/gundo
-git submodule add https://github.com/vim-scripts/TaskList.vim.git bundle/tasklist
 git submodule add https://github.com/vim-scripts/The-NERD-tree.git bundle/nerdtree
 git submodule add https://github.com/scrooloose/syntastic.git bundle/syntastic
 git submodule add -f git://github.com/rodjek/vim-puppet.git bundle/puppet
